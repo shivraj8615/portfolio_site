8d2c653bae6846d0dd22de12a44da496ad60357c
shivraj8615 <75648551+shivraj8615@users.noreply.github.com>
2023-03-11 17:38:37 +0000

    added path in flask file

--- app.py
+++ app.py
@@ -4,7 +4,23 @@ app = Flask(__name__)
 
 @app.route("/")
 #home page
-def hello_world():
+def home_view():
   return render_template('home.html')
+@app.route("/about")
+#about page
+def about_view():
+  return render_template('about.html')
+@app.route("/blog")
+#blog list_page
+def blog_list():
+  return render_template('blog.html')
+@app.route("/portfolio")
+# project portfolio list
+def portfolio_list():
+  return render_template('portfolio.html')
+@app.route("/contact")
+#contact page
+def contact_us():
+  return render_template('contact.html')
 if __name__ == "__main__":
   app.run(host="0.0.0.0",debug=True)
